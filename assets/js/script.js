56d1d4db9c630c18aaa37fc1ced7bf1c81a8784f
ismail280701 <ismail2807011@gmail.com>
2026-08-09 20:35:58 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -311,3 +311,37 @@ button.innerHTML =
 }
 
 }
+/* =========================================
+ANIMASI SCROLL
+========================================= */
+
+const observer =
+new IntersectionObserver(
+
+(entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+},
+
+{
+threshold:0.15
+}
+
+);
+
+document
+.querySelectorAll(".fade-up")
+.forEach(el=>{
+
+observer.observe(el);
+
+});
