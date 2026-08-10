57a7555d8b61e32a205b38ff022bb5d038d64026
ismail280701 <ismail2807011@gmail.com>
2026-08-10 13:57:09 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 /* =====================================
    LOADING SCREEN
 ===================================== */
@@ -434,6 +436,49 @@ button.textContent =
 "Baca Selengkapnya";
 
 }
+
+});
+
+});
+/* =========================================
+FILTER GALERI
+========================================= */
+
+const filterButtons =
+document.querySelectorAll(".gallery-filter-btn");
+
+const galleryItems =
+document.querySelectorAll(".gallery-item");
+
+filterButtons.forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+filterButtons.forEach(btn=>
+btn.classList.remove("active")
+);
+
+button.classList.add("active");
+
+const filter =
+button.getAttribute("data-filter");
+
+galleryItems.forEach(item=>{
+
+if(
+filter==="all" ||
+item.dataset.category===filter
+){
+
+item.style.display="block";
+
+}else{
+
+item.style.display="none";
+
+}
+
+});
 
 });
 
