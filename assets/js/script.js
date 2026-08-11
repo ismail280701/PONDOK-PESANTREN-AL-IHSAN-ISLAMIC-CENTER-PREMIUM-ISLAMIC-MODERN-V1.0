e779cf2c2a83184d5a41aa117cf4d5b87d074945
ismail280701 <ismail2807011@gmail.com>
2026-08-11 20:59:44 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,21 +35,6 @@ function toggleMenu() {
    STICKY HEADER
 ===================================== */
 
-window.addEventListener("scroll", () => {
-
-    const header =
-        document.querySelector("header");
-
-    if(!header) return;
-
-    if(window.scrollY > 50){
-        header.classList.add("scrolled");
-    }else{
-        header.classList.remove("scrolled");
-    }
-
-});
-
 
 /* =====================================
    SCROLL REVEAL
